57a7f839685a6165161c131268189758d841b1d7
Milton Orellana <go0211032020@unab.edu.sv>
2021-03-04 17:14:31 -0600

    VISTA

--- Lab1App/Lab1App/VISTA/Menu.Designer.cs
+++ Lab1App/Lab1App/VISTA/Menu.Designer.cs
@@ -35,11 +35,14 @@ namespace Lab1App
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.BackColor = System.Drawing.Color.SkyBlue;
-            this.button1.Location = new System.Drawing.Point(100, 106);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(133, 130);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(167, 150);
+            this.button1.Size = new System.Drawing.Size(223, 185);
             this.button1.TabIndex = 1;
             this.button1.Text = "USUARIOS";
             this.button1.UseVisualStyleBackColor = false;
@@ -47,11 +50,14 @@ namespace Lab1App
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.BackColor = System.Drawing.Color.SteelBlue;
-            this.button2.Location = new System.Drawing.Point(287, 106);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(383, 130);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(158, 150);
+            this.button2.Size = new System.Drawing.Size(211, 185);
             this.button2.TabIndex = 2;
             this.button2.Text = "ADMINISTRADOR";
             this.button2.UseVisualStyleBackColor = false;
@@ -59,16 +65,16 @@ namespace Lab1App
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.IsMdiContainer = true;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "MENU";
             this.ResumeLayout(false);
 
         }
